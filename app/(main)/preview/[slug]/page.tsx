ce21e8c24af1608fad2e8c04e2ff96e083905b34
in-deepaknegi <negideepak.in@gmail.com>
2025-07-20 23:53:23 +0530

    update: #22 - added 3 pricing components

--- app/(main)/preview/[slug]/page.tsx
+++ app/(main)/preview/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Index } from "@/__registry__";
-import { Metadata } from "next";
 
 type Params = Promise<{ slug: string }>;
 
@@ -8,13 +7,14 @@ const PreviewPage = async ({ params }: { params: Params }) => {
     const slug = await params;
     console.log(slug);
 
-    const Preview = Index["default"][slug.slug as keyof typeof Index["default"]]?.component;
+    const Preview =
+        Index["default"][slug.slug as keyof (typeof Index)["default"]]
+            ?.component;
 
-    // If Preview is a valid component, render it using React.Suspense
     if (!Preview) {
         return (
             <>
-            <div>DDDD</div>
+                <div>DDDD</div>
             </>
         );
     }
@@ -24,16 +24,15 @@ const PreviewPage = async ({ params }: { params: Params }) => {
             <section>
                 <React.Suspense
                     fallback={
-                        <div className="flex items-center text-sm text-muted-foreground">
+                        <div className="text-muted-foreground flex items-center text-sm">
                             Loading...
                         </div>
                     }
                 >
                     <div className="overflow-col-span-2 row-start-2 mx-auto min-w-0">
                         <div
-                            className={`overflow-y-auto break-words bg-white font-ins font-medium`}
+                            className={`font-ins overflow-y-auto bg-white font-medium break-words`}
                         >
-                            {/* Render the component */}
                             <Preview />
                         </div>
                     </div>
